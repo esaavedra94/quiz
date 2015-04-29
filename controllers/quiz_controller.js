3baa99b8d440cddc1be84bcfe8e1fa9fa203bb71
Edgar Saavedra <esaavedra@outlook.com>
2015-04-29 18:43:37 +0200

    bd incluidas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,6 @@
+var models = require('../models/models.js');
+
+/*
 // GET /quizes/question
 exports.question = function(req, res) {
   res.render('quizes/question', {pregunta: 'Capital de Italia'});
@@ -11,4 +14,24 @@ exports.answer = function(req, res) {
   else {
     res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
+};
+*/
+
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+  res.render('quizes/question', {pregunta: quiz[0].pregunta});
+})
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', {respuesta: 'Correcto'});
+    }
+    else {
+      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+  })
 };
