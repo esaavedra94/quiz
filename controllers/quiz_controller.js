91cdaf2d665c2f241772eeaa25eff58d55ec75c9
Edgar Saavedra <esaavedra@outlook.com>
2015-05-15 12:25:11 +0200

    crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,6 +17,7 @@ exports.answer = function(req, res) {
 };
 */
 
+// Autoload, factoriza el código si ruta incluye :quizId
 exports.load= function(req, res, next, quizId){
   models.Quiz.find(quizId).then(
     function(quiz){
@@ -55,14 +56,34 @@ exports.show = function(req, res) {
 })
 };
 
-// GET /quizes/answer
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === req.quiz.respuesta) {
-      res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Correcto'});
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer',
+    { quiz: req.quiz,
+      respuesta: resultado
     }
-    else {
-      res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto'});
-    }
-  })
+  );
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })   // res.redirect: Redirección HTTP a lista de preguntas
 };
