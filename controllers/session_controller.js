cfae54cd9d3585d2c82502863f2c7ae021b65fef
Edgar Saavedra <esaavedra@outlook.com>
2015-05-26 17:01:56 +0200

    aviso de expiracion de sesion por autologout

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -46,6 +46,9 @@ exports.destroy = function(req, res) {
 
 //autologout
 exports.autologout = function(req, res, next) {
-  if (req.session.user && ((req.session.user.ei - req.session.user.eo) > 120000)) res.redirect('/logout');
+  if (req.session.user && ((req.session.user.ei - req.session.user.eo) > 120000)) {
+    req.session.message = true;
+    res.redirect('/logout');
+  }
   else next();
 }
